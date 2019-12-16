6a5c8b50b0fbb363f778011648ac0bbba33a202c
유제혁 <jhyoo@vetec.co.kr>
2019-12-16 13:47:23 +0900

    express_test

---
+++ NodejsConsoleApp1/NodejsConsoleApp1/express_test.js
@@ -0,0 +1,10 @@
+﻿var express = require("express"); 
+var app = express();
+
+app.get("/", function (request, response) {
+    response.send("Hello World");
+});
+
+app.listen(3000, function () {
+    console.log("Express app started on port 3000.");
+});
